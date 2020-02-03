738709773cdeba424f3b765a05b4e3f4f5818bde
GTK <gaurav21776@gmail.com>
2020-02-03 17:45:37 -0500

    Code update based on piazzza

--- assign1/prog1.cpp
+++ assign1/prog1.cpp
@@ -1,23 +1,27 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define debug 0
 #include <bits/stdc++.h>
 using namespace std;
-//Generates random numbers and compute their sum
-double compute_local_sum(int N, long int C, int rank, int size){
+/*
+Debug code runs just if the flag is on; Setting it to 0 by default
+*/
+#define debug 0
+
+//Generates random numbers array
+void populate_random_array(int N, long int C, int rank, int size, double* arr){
     long int seed = C + rank;
     srand48(seed);
     int n = N / size;
-    double sum = 0.0;
     for (int i = 0; i < n; i++){
-        if(debug)
-            sum += (rank*n + i+1);
+       if(debug)
+            arr[i] = (rank*n + i+1);
         else
-            sum += drand48();
+            arr[i] = drand48();
     }
-    return sum;
+    return;
 }
+
 int main(int argc, char** argv) {
   
     MPI_Init(&argc, &argv);
@@ -47,21 +51,32 @@ int main(int argc, char** argv) {
         if(debug)
             cout << "rank " << rank << " "<< N << " " << C << endl;
     }
-
+    
+    //Braodcasting the N,C values to all the processors
     MPI_Bcast(payload, 2, MPI_INT, 0, MPI_COMM_WORLD);
 
     double time;
-    if(!rank)
-	time = MPI_Wtime();
-    double local_sum = compute_local_sum(payload[0], payload[1], rank, size);
+    //Start the time clock
+    time = MPI_Wtime();
     
-    //if(debug)
-       // cout << "rank " << rank << " "<< local_sum << endl;
- 
+    //Populate a random array 
+    N = payload[0];
+    C = payload[1];
+    int sz = N/size;
+    double *arr;
+    arr = (double *)calloc(sz, sizeof(double));
+    populate_random_array(N, C, rank, size, arr);
+    
+    //Computing local sum of the random array
+    double local_sum = 0.0;
+    for(int i=0; i<sz; i++)
+        local_sum += arr[i];
+    
+    //Algorithm as discussed in the class
     int j = 1;
     while ( j < size)
     {
-	int dst = rank ^ j;
+    int dst = rank ^ j;
         if((rank & j) != 0){
             if(debug)
                cout << "Src " << rank << " Des "<< dst << endl;
@@ -70,16 +85,23 @@ int main(int argc, char** argv) {
         }
         else{
             double tmp;
-	    MPI_Status status;
+            MPI_Status status;
             MPI_Recv(&tmp, 1, MPI_DOUBLE, dst, 99, MPI_COMM_WORLD, &status);
             local_sum += tmp;
         }
         j = j << 1;
     }
     
+    //free the array, as we no longer need it
+    free(arr);
+    
+    time = MPI_Wtime() - time;
+    //using MPI_reduce to calculate max of all the times
+    double max_time;
+    MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+        
     if(!rank){
-        time = MPI_Wtime() - time;
-        printf("N= %d, P= %d, C= %d, S= %lf \nTime= %12.5e\n", N, size, C, local_sum, time);
+        printf("N= %d, P= %d, C= %d, S= %lf \nTime= %12.5e\n", N, size, C, local_sum, max_time);
     }
     MPI_Finalize();
     return 0;
